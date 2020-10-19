71aff4b5a09d5ade3f69ec14f04b80a6c2b1c2ad
Karl Behrens <karlbehrensg@gmail.com>
2020-10-19 20:43:53 -0300

    Escribiendo json en text area

--- karate/views.py
+++ karate/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 
-def json_fields(request):
-    return render(request, 'karate/json_fields.html')
+def json_fields(request, json_origin=None):
+    fields = json_origin
+    return render(request, 'karate/json_fields.html', {'fields': fields})
