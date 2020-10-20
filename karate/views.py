f6a2d8e4eb1e061484f6726e8ef492bbfb963aca
Karl Behrens <karlbehrensg@gmail.com>
2020-10-20 01:38:16 -0300

    Renderizando arbol de diccionario

--- karate/views.py
+++ karate/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 import json
 
 
+def take_fields(data):
+    fields = {}
+    for key in data:
+        if type(data[key]) == list and type(data[key][0]) == dict:
+            for item in data[key]:
+                fields[key] = take_fields(item)
+        else:
+            fields[key] = None
+    return fields
+
+
 def json_fields(request):
 
     fields = None
@@ -9,5 +20,6 @@ def json_fields(request):
     if request.method == 'POST' and request.POST['content']:
         fields = request.POST['content']
         fields = json.loads(fields)
+        fields = take_fields(fields)
 
     return render(request, 'karate/json_fields.html', {'fields': fields})
