3489afc1e8520cd2410c642e133d90db87485a16
Karl Behrens <karlbehrensg@gmail.com>
2020-10-19 21:15:18 -0300

    Obteniendo JSON e imprimiendolo en pantalla

--- karate/views.py
+++ karate/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+import json
 
 
-def json_fields(request, json_origin=None):
-    fields = json_origin
+def json_fields(request):
+
+    fields = None
+
+    if request.method == 'POST':
+        fields = request.POST['content']
+
     return render(request, 'karate/json_fields.html', {'fields': fields})
