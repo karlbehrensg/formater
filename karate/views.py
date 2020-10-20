e2464eb9cd1f078f40d550405b36eda66924ae1b
Karl Behrens <karlbehrensg@gmail.com>
2020-10-19 23:14:40 -0300

    Extrayendo campos de JSON e imprimiendo en pantalla

--- karate/views.py
+++ karate/views.py
@@ -6,7 +6,8 @@ def json_fields(request):
 
     fields = None
 
-    if request.method == 'POST':
+    if request.method == 'POST' and request.POST['content']:
         fields = request.POST['content']
+        fields = json.loads(fields)
 
     return render(request, 'karate/json_fields.html', {'fields': fields})
